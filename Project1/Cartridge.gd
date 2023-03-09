7a09b7a3eefc2f2f78e37c2ebc60862ba95b2293
Sonzoran <50843400+Sonzoran@users.noreply.github.com>
2023-03-09 13:28:11 +0100

    test2

---
+++ Project1/Cartridge.gd
@@ -0,0 +1,14 @@
+extends Node3D
+
+@export var speed = 70
+
+const KILL_BULLET = 2
+var timer = 0
+# Called when the node enters the scene tree for the first time.
+func _physics_process(delta):
+	var forward_direction = global_transform.basis.z.normalized()
+	global_translate(forward_direction * speed * delta)
+	
+	timer += delta
+	if timer >= KILL_BULLET:
+		queue_free()
