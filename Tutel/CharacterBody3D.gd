b7f8eff1be9f06bf5fbee74f4e4dd5e7e2847e6b
Marsianer03 <marsianer03@gmail.com>
2023-04-22 12:59:19 +0200

    player spawns at path_end fix map generation

--- Tutel/CharacterBody3D.gd
+++ Tutel/CharacterBody3D.gd
@@ -4,20 +4,29 @@ extends CharacterBody3D
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 
+var map_generator = GenerateMap.new()
+var spawn_position = map_generator.path_end
+
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = 9.8
 
 @onready var camera = $Camera3D
 var param = PhysicsRayQueryParameters3D.new()
 
+func _ready():
+	#Spawnt den Character an genannter Positiond
+	transform.origin = spawn_position
+
+
 func _physics_process(delta):
 	# get current physical state.
 	var space_state = get_world_3d().direct_space_state
+
 	
 	
 	# get current mouse position.
 	var mouse_position = get_viewport().get_mouse_position()
-	print(mouse_position)
+	#print(mouse_position)
 	param.from = camera.project_ray_origin(mouse_position)
 	param.to = param.from + camera.project_ray_normal(mouse_position) * 2000
 	
