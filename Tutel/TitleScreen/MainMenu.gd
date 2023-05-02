880e71a42a0d48d9486d08b6e13d5d31aa9b8e14
Alecubeci <alexander.s23@schule.bremen.de>
2023-05-02 02:14:02 +0200

    Option Menu with Master Volume and a whole lotta BRICKS

--- Tutel/TitleScreen/MainMenu.gd
+++ Tutel/TitleScreen/MainMenu.gd
@@ -1,11 +1,14 @@
-extends Node2D
+extends Control
 
 @export var MainGameScene : PackedScene
-#@export var OptionsScene : PackedScene
 
-func _on_new_game_button_button_up():
+func _on_new_game_button_pressed():
 	get_tree().change_scene_to_file(MainGameScene.resource_path)
 
 
-#func _on_options_button_button_up():
-#	get_tree().change_scene_to_file()
+func _on_options_button_pressed():
+	get_tree().change_scene_to_file("res://Options/OptionMenu.tscn")
+
+
+func _on_quit_button_pressed():
+	get_tree().quit()
