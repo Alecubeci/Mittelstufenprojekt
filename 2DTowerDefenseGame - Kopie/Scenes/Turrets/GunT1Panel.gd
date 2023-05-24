77ec3c94285765371241f03e694caa0c4b39d75d
Alebrosan Schäfer <Alebrosan@yahoo.de>
2023-05-24 22:07:23 +0200

    Tower mit Area

--- 2DTowerDefenseGame - Kopie/Scenes/Turrets/GunT1Panel.gd
+++ 2DTowerDefenseGame - Kopie/Scenes/Turrets/GunT1Panel.gd
@@ -15,11 +15,28 @@ func _on_gui_input(event):
 		
 		get_child(1).global_position = event.global_position
 		
-	elif event is InputEventMouseButton and event.button_mask == 0:
-		get_child(1).queue_free()
-		var path = get_tree().get_root().get_node("GameScene")
+		var mapPath = get_tree().get_root().get_node("GameScene/Map1/EverythingElse")
+		var tile = mapPath.local_to_map(get_global_mouse_position())
+		currTile = mapPath.get_cell_atlas_coords(0 , tile, false)
+		if currTile == Vector2i(0,0):
+			get_child(1).get_node("Area").modulate = Color(255,255,255)
+		else:
+			get_child(1).get_node("Area").modulate = Color(0,255,0)
 		
-		path.add_child(tempTower)
-		tempTower.global_position = event.global_position
-	
+	elif event is InputEventMouseButton and event.button_mask == 0:
+		if event.global_position.x >= 1150 and event.global_position.y >= 500:
+			if get_child_count() > 1:
+				get_child(1).queue_free()
+		else: 
+			if get_child_count() > 1:
+				get_child(1).queue_free()
+			if (currTile == Vector2i(0,0)):
+				var path = get_tree().get_root().get_node("GameScene")
+		
+				path.add_child(tempTower)
+				tempTower.global_position = event.global_position
+				tempTower.get_node("Area").hide()
+	else:
+		if get_child_count() > 1:
+			get_child(1).queue_free()
 
