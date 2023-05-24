327384e2c5442cd4500ad854a60920ba30337cc6
Alebrosan Schäfer <Alebrosan@yahoo.de>
2023-05-24 22:21:13 +0200

    1

--- 2DTowerDefenseGame - Kopie/Scenes/Turrets/GunT1Panel.gd
+++ 2DTowerDefenseGame - Kopie/Scenes/Turrets/GunT1Panel.gd
@@ -18,7 +18,7 @@ func _on_gui_input(event):
 		var mapPath = get_tree().get_root().get_node("GameScene/Map1/EverythingElse")
 		var tile = mapPath.local_to_map(get_global_mouse_position())
 		currTile = mapPath.get_cell_atlas_coords(0 , tile, false)
-		if currTile == Vector2i(0,0):
+		if currTile == Vector2i(0,0) or currTile == Vector2i(0,1):
 			get_child(1).get_node("Area").modulate = Color(255,255,255)
 		else:
 			get_child(1).get_node("Area").modulate = Color(0,255,0)
@@ -31,6 +31,8 @@ func _on_gui_input(event):
 			if get_child_count() > 1:
 				get_child(1).queue_free()
 			if (currTile == Vector2i(0,0)):
+				get_child(1).queue_free()
+			else:
 				var path = get_tree().get_root().get_node("GameScene")
 		
 				path.add_child(tempTower)
