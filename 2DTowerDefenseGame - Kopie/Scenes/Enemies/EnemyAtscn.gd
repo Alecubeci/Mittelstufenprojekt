d718a3ffe6e3ed4719282c3cb389dad045a767c0
Alebrosan Schäfer <Alebrosan@yahoo.de>
2023-05-31 21:10:04 +0200

    Tower can shoot at target

--- 2DTowerDefenseGame - Kopie/Scenes/Enemies/EnemyAtscn.gd
+++ 2DTowerDefenseGame - Kopie/Scenes/Enemies/EnemyAtscn.gd
@@ -1,9 +1,13 @@
 extends CharacterBody2D
 
 @export var speed = 100
+var health = 15
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	get_parent().set_progress(get_parent().get_progress() + speed * delta)
 	if get_parent().get_progress_ratio() == 1:
 		queue_free()
+		
+	if health <= 0:
+		get_parent().get_parent().queue_free()
--- 2DTowerDefenseGame - Kopie/Scenes/Turrets/GunT2.gd
+++ 2DTowerDefenseGame - Kopie/Scenes/Turrets/GunT2.gd
@@ -1,1 +1,19 @@
-extends "res://Scenes/Turrets/Turrets.gd"
+extends StaticBody2D
+
+var bullet = preload("res://Scenes/Turrets/Gun1Bullet.tscn")
+var bulletDamage = 5
+var pathName
+var currTarget = []
+var curr
+
+
+func _on_tower_body_entered(body):
+		if "EnemyA" in body.name:
+			var tempArray = []
+			currTarget = get_node("Tower").get_overlapping_bodies()
+			
+		print(currTarget)
+
+
+func _on_tower_body_exited(body):
+	pass # Replace with function body.
